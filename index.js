785f30e3474c2b64dcf5d1eb0e34e6c03389c751
Eduardo Russo <russoedu@gmail.com>
2020-02-25 10:19:06 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+// require("./src/ex-01-paragraph_repetitions.js");
+// require("./src/ex-02-fill_with_interpolation.js");
+// require("./src/ex-03-maximum_profit.js");
+// require("./src/ex-04-stack_from_queues.js");
+// require("./src/ex-05-queue_from_stacks.js");
+// require("./src/ex-06-bubble_sort.js");
+// require("./src/ex-07-merge_sort.js");
+// require("./src/ex-08-counting_sort.js");
+// require("./src/ex-09-overlaping_rectangles.js");
+// require("./src/ex-10-quick_sort.js");
+// require("./src/ex-11-min_max_in_array.js");
+// require("./src/ex-12-is_prime.js");
+// require("./src/ex-13-reverse_linked_in_place.js");
+// require("./src/ex-14-sum_from_ll.js");
+// require("./src/ex-15-max_no_repeat.js");
+// require("./src/leetcode.js");
+
+require("./src/leetcode.js");
